7832bb5ec21bed1f24e2824491d4e36d68495abe
cXiaof <310257509@qq.com>
2018-08-20 11:04:34 +0800

    publish alpha.2

--- dist/maptalks.autogradual.mjs
+++ dist/maptalks.autogradual.mjs
@@ -1,5 +1,5 @@
 /*!
- * maptalks.autogradual v0.1.0-alpha.1
+ * maptalks.autogradual v0.1.0-alpha.2
  * LICENSE : MIT
  * (c) 2016-2018 maptalks.org
  */
@@ -3605,4 +3605,4 @@ AutoGradual.mergeOptions(options);
 
 export { AutoGradual };
 
-typeof console !== 'undefined' && console.log('maptalks.autogradual v0.1.0-alpha.1');
+typeof console !== 'undefined' && console.log('maptalks.autogradual v0.1.0-alpha.2');
