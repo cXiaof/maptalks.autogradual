467e8bf1b99e2e038eb250f268c768968a13aef4
cXiaof <310257509@qq.com>
2019-04-17 21:40:40 +0800

    add a tip panel in demo. Released 0.1.0 without beta/alpha

--- demo/main.js
+++ demo/main.js
@@ -12,12 +12,14 @@ const map = new maptalks.Map('map', {
 const ag = new maptalks.AutoGradual()
 const layer = new maptalks.VectorLayer('sketchPad').addTo(map)
 
+// new drawtool
 const drawTool = new maptalks.DrawTool({ mode: 'Polygon' }).addTo(map).disable()
 drawTool.on('drawend', (param) => {
     const { geometry } = param
     geometry.addTo(layer)
 })
 
+//new toolbar
 const modes = ['Point', 'LineString', 'Polygon', 'Rectangle', 'Circle', 'Ellipse']
 let children = []
 modes.map((item) =>
@@ -47,3 +49,21 @@ const toolbar = new maptalks.control.Toolbar({
         }
     ]
 }).addTo(map)
+
+//tip panel
+const textPanel = new maptalks.control.Panel({
+    position: 'bottom-left',
+    draggable: true,
+    custom: false,
+    content: `
+        Click a type in <b>Draw</b> to draw sth geos,<br />
+        and also can draw other type geos if you want.<br />
+        Do click <b>Gradual Change</b> to see the effect.<br />
+        <br />
+        点击<b>Draw</b>里的类型开始画一些相应的图形，<br />
+        也可以继续画其他的类型的图形。<br />
+        然后点击<b>Gradual Change</b>查看效果。
+    `,
+    closeButton: true
+})
+map.addControl(textPanel)
